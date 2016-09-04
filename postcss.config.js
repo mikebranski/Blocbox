e0ee0ab1f95d4361896276323fe278a6bf7e6ff3
Mike Branski <mikebranski@gmail.com>
2016-09-04 13:26:10 -0500

    Add PostCSS mixins and disable px-to-em

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,18 @@
 module.exports = {
   "use": [
     "postcss-cssnext",
+    // This has to be loaded before postcss-simple-vars and postcss-nested.
+    "postcss-mixins",
     "postcss-nesting",
     "postcss-normalize",
-    "postcss-px-to-em"
+    // This isn't smart enough to skip things like borders, at least not without
+    // additional configuration. Requires further investigation.
+    // "postcss-px-to-em"
   ],
   "input": "css/styles.css",
   "dir": "build",
   "watch": true,
-  "postcss-px-to-em": {
-    base: 16
-  }
+  // "postcss-px-to-em": {
+  //   base: 16
+  // }
 };
